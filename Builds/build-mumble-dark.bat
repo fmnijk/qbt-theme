25b51f968ec719a6121e58f2f58f19119547bc6b
fmnijk <fmnijk@gmail.com>
2023-09-12 22:36:34 +0900

    modified

--- Builds/build-mumble-dark.bat
+++ Builds/build-mumble-dark.bat
@@ -1,13 +1,16 @@
-
+@echo off
 set SCRIPT_DIR=mumble-theme
-set PATH=%PATH%;C:\Users\Prince\Downloads\Compressed\qtsass_0.2.2_win64;
+set PATH=%PATH%
 pushd %SCRIPT_DIR%\source
-qtsass -o ../Dark.qss Dark.scss
 popd
 rd /q /s mumble-icons
 mkdir mumble-icons
 python fill-icons.py nowshed #4B9CD3 mumble-icons
-# for /r icons/nowshed/ %%i in (*.svg) do powershell -Command "(gc %%~dpnxi) -replace '#4285f4', '#6a1b9a' | Out-File -encoding ASCII mumble-icons/%%~nxi"
+for /r icons/nowshed/ %%i in (*.svg) do powershell -Command "(gc %%~dpnxi) -replace '#4285f4', '#6a1b9a' | Out-File -encoding ASCII mumble-icons/%%~nxi"
 python make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -icons-dir mumble-icons -output mumble-dark-nowshed-recolored -style dark.qss 
 rd /q /s mumble-icons
-python make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -icons-dir icons/nowshed -output mumble-dark-nowshed -style dark.qss 
+python make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -icons-dir icons/nowshed -output mumble-dark-nowshed -style dark.qss
+
+copy /y mumble-dark-nowshed.qbtheme %LOCALAPPDATA%\qBittorrent
+move /y mumble-dark-nowshed.qbtheme ..\mumble-dark-nowshed.qbtheme
+move /y mumble-dark-nowshed-recolored.qbtheme ..\mumble-dark-nowshed-recolored.qbtheme
